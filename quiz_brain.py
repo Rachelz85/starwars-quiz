f229deac247cf3e735d1a81f07bf9cc93fbd1760
Rachelz85 <darthrachelz@gmail.com>
2024-06-11 12:43:46 +0000

    Add method to check if the answer matches the correct current answer and print messages for correct or incorrect answers and also the correct answer itself

--- quiz_brain.py
+++ quiz_brain.py
@@ -15,6 +15,7 @@ class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
+        # Will add score atribute here 
         self.question_list = q_list
 
     # Still had questions method, 
@@ -24,4 +25,13 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1 # Increment the question number
-        input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        self.check_answer(user_answer, current_question.answer)
+
+    # method to check if the answer matches the correct current answer 
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("Correct you are, young Padawan. The Force is strong with you!")
+        else:
+            print("Incorrect, young Padawan. Much to learn, you still have")
+        print(f"The correct answer was: {correct_answer}.")
