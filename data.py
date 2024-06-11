5a5dce328591c7e04763cdc8b5166cbd93cb3802
Rachelz85 <darthrachelz@gmail.com>
2024-06-11 12:15:45 +0000

    Create a questions list of dictionaries in the data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+{"text": "Obi-Wan Kenobi's lightsaber was blue.", "answer": "True"},
+{"text": "The Sith homeworld is known as Mustafar.", "answer": "False"},
+{"text": "C-3PO And R2-D2 Are The First Characters To Appear In Star Wars.", "answer": "True"},
+{"text": "Lando Calrissian Destroyed The Second Death Star.", "answer": "True"},
+{"text": "Luke Skywalker Grew Up On A Moisture Farm.", "answer": "True"},
+{"text": "Princess Leia Is Dropped In The Rancor Pit.", "answer": "False"},
+{"text": "Count Dooku's Sith name is Darth Tyranus", "answer": "True"},
+{"text": "C-3PO Is Fluent In Over 6 Billion Forms Of Communication.", "answer": "False"},
+{"text": "Chewbacca's Home Planet Is Kashyyyk.", "answer": "True"},
+{"text": "Chancellor Palpatine Kills Three Other Jedi Before Fighting Mace Windu.", "answer": "True"},
+{"text": "Anakin Kills Count Dooku With Both His Own AND Obi-Wan's Lightsaber.", "answer": "False"},
+{"text": "Han Solo Is The Only Non-Jedi To Use A Lightsaber In The Original Trilogy.", "answer": "True"}
+]
